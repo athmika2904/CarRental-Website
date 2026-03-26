23e8569506d5f822df0e4bebb816b3eddd699657
athmika2904 <athmikagowda2006@gmail.com>
2026-03-26 22:29:40 +0530

    routes added

--- clientpage/src/App.jsx
+++ clientpage/src/App.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-
+import { useLocation,Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Cars from './pages/Cars';
+import carDetails from './pages/carDetails';
+import MyBookings from './pages/MyBookings';
 const App = () => {
+  const [showlogin,setShowLogin]=useState(false);
+  const isOwnerpath=useLocation().pathname.startsWith('/owner')
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <>
+      {!isOwnerpath && <Navbar setShowLogin={setShowLogin}/>}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details' element={<carDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBookings/>}/>
+      </Routes>
+    </>
   )
 }
 

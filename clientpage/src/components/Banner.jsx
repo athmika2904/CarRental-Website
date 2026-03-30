6f34c6d333e17f0f05445757ebbeca0eccee8b91
athmika2904 <athmikagowda2006@gmail.com>
2026-03-30 19:09:36 +0530

    Banner to list cars implemented

--- clientpage/src/components/Banner.jsx
+++ clientpage/src/components/Banner.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
-
+import { assets } from '../assets/assets'
 const Banner = () => {
   return (
-    <div className='flex flex-col md:flex-row md:items-start items-center justify-between
-    px-8 md:pl-14 pt-10 bg-linear-to-r from-[#0558FE] to-[#A9CFFF] max-w-6xl mx-3
-    md:mx-auto rounded-2xl overflow-hidden'>
-      
+    <div className='flex flex-col md:flex-row items-center md:items-start justify-between
+    px-8 md:px-14 py-10
+    bg-linear-to-r from-[#0558FE] to-[#A9CFFF]
+    max-w-4xl mx-3 md:mx-auto
+    rounded-2xl overflow-hidden'>
+
+      <div className='text-white max-w-lg'>
+        <h2 className='text-3xl md:text-4xl font-semibold'>
+          Own a Luxury Car?
+        </h2>
+
+        <p className='mt-3 text-sm md:text-base'>
+          Turn your premium vehicle into a steady source of income.
+        </p>
+
+        <p className='mt-1'>
+          List your car with us and we handle bookings, driver verification,
+          insurance, and secure payments.
+        </p>
+
+        <button
+          className='px-6 py-2 bg-white hover:bg-slate-100
+          transition-all text-blue-600 rounded-lg
+          text-sm mt-5 cursor-pointer'>
+          List your car
+        </button>
+      </div>
+
+      <img
+        src={assets.banner_car_image}
+        alt="car"
+        className='max-h-48 md:max-h-52 mt-8 md:mt-0'
+      />
+
     </div>
   )
 }
--- clientpage/src/pages/Home.jsx
+++ clientpage/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../components/Hero'
 import Features from '../components/Features'
+import Banner from '../components/Banner'
 
 const Home = () => {
   return (
     <>
       <Hero/>
       <Features/>
+      <Banner/>
     </>
   )
 }

73610410136f56239e524c7f5c97630845b96700
athmika2904 <athmikagowda2006@gmail.com>
2026-04-21 22:32:21 +0530

    car managing page added

--- clientpage/src/pages/Owner/ManageCars.jsx
+++ clientpage/src/pages/Owner/ManageCars.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { assets, dummyCarData } from '../../assets/assets'
+import TitleOwner from '../../components/Owner/TitleOwner'
 
 const ManageCars = () => {
+  const [cars,setCars]=useState([])
+  const fetchCar=async()=>{
+    setCars(dummyCarData)
+  }
+  useEffect(()=>{
+    fetchCar()
+  },[])
   return (
-    <div>
-      
+    <div className='px-4 pt-10 md:px-10 w-full'>
+      <TitleOwner title="Manage Cars" subtitle="View all listed cars, 
+      update their details, or remove them from the booking platform."/>
+      <div className='max-w-3xl w-full rounded-md overflow-hidden border border-borderColor mt-6'>
+        <table className='w-full border-collapse text-left text-sm text-gray-600'>
+          <thead className='text-gray-500'>
+            <tr>
+              <th className='p-3 font-medium'>Car</th>
+              <th className='p-3 font-medium max-md:hidden'>Category</th>
+              <th className='p-3 font-medium'>Price</th>
+              <th className='p-3 font-medium max-md:hidden'>Status</th>
+              <th className='p-3 font-medium'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cars.map((c,index)=>(
+              <tr key={index} className='border-t border-borderColor'>
+                <td className='p-3 flex items-center gap-3'>
+                  <img src={c.image} alt="" className='h-12 w-12 aspect-square
+                  rounded-md object-cover'/>
+                  <div className='max-md:hidden'>
+                    <p className='font-medium'>{c.brand} {c.model}</p>
+                    <p className='text-xs text-gray-500'>{c.seating_capacity} . {c.transmission} </p>
+                  </div>
+                </td>
+                <td className='p-3 max-md:hidden'>
+                  {c.category}
+                </td>
+                <td className='p-3'>₹{c.pricePerDay}/day</td>
+                <td lassName='p-3 max-md:hidden'>
+                  <span className={`px-3 py-1 rounded-full text-xs ${c.isAvailable?'bg-green-100 text-green-500':
+                    'bg-red-100 text-red-500'
+                  }`}>
+                    {c.isAvailable?"Available":"Unavailable"}
+                  </span>
+                </td>
+                <td className='flex items-center p-3'>
+                    <img src={c.isAvailable?assets.eye_close_icon:assets.eye_icon} alt="" 
+                    className='cursor-pointer'/>
+                    <img src={assets.delete_icon} alt="" 
+                    className='cursor-pointer'/>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }

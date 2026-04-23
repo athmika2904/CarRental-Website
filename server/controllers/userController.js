b762f0f93907fce5e2a5798b94c29008b7d85460
athmika2904 <athmikagowda2006@gmail.com>
2026-04-24 00:40:43 +0530

    backend and database setup

---
+++ server/controllers/userController.js
@@ -0,0 +1,46 @@
+import user from "../models/User.js"
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken";
+const generateToken=(userId)=>{
+    const payload=userId;
+    return jwt.sign(payload,process.env.SECRET_KEY)
+}
+export const registeruser=async(req,res)=>{
+    try{
+        const{name,email,password}=req.body
+        if(!name||!email||!password||password.length<8){
+            return res.json({success:false,message:"Fill all the fields"})
+        }
+        const userExists=await user.findOne({email})
+        if(userExists){
+            return res.json({success:false,message:"User already exists"})
+        }
+        const hashedPassword=await bcrypt.hash(password,10)
+        const usermodel=await user.create({name,email,password:hashedPassword})
+        const token=generateToken(user._id.toString());
+        res.json({success:true,token})
+
+    }catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
+export const LoginUser=async(req,res)=>{
+    try{
+        const {email,password}=req.body
+        const usermodel=await user.findOne({email})
+        if(!user){
+            return res.json({success:false,message:"User not found"})
+        }
+        const isMatch=await bcrypt.compare(password,usermodel.password)
+        if(!isMatch){
+            return res.json({success:false,message:"Invalid credentials"})
+        }
+        const token=generateToken(user._id.toString());
+        res.json({success:true,token})
+    }
+    catch(error){
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+}
---
+++ server/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+const app=express();
+await connectDB();
+app.use(cors());
+app.use(express.json());
+app.get('/',(req, res)=>res.send("Server is running"));
+const PORT=process.env.PORT||5000;
+app.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
